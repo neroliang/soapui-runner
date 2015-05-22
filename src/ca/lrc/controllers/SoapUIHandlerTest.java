7aaf2c5ab3286dccff8ea6947eea18907a1dbbbb
Daniel Vu <duongxvu@gmail.com>
2015-05-22 15:06:13 -0400

    JUnit test now compares expected result against actual result properly. Result and Service objects are being populated as intended as well by the runTestSuite method in the SoapUIHandler class.

--- src/ca/lrc/controllers/SoapUIHandlerTest.java
+++ src/ca/lrc/controllers/SoapUIHandlerTest.java
@@ -1,11 +1,12 @@
 package ca.lrc.controllers;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,14 +17,10 @@ public class SoapUIHandlerTest {
 	private List<Service> testStatusServiceList;
 	private Result expectedResult;
 
-	@Before
-	public void setUp() throws Exception {
-		expectedResult = new Result();
-		testStatusServiceList = new ArrayList<Service>();
-	}
-
 	@Test
 	public void testRunTestSuite() throws Exception {
+		expectedResult = new Result();
+		testStatusServiceList = new ArrayList<Service>();
 		Collections.addAll(testStatusServiceList, new Service("CommonLookup",
 				true), new Service("EasrInternal", true), new Service(
 				"EasrSubmission", true),
@@ -32,11 +29,22 @@ public class SoapUIHandlerTest {
 				new Service("CamsManagement", true), new Service("CamsProfile",
 						true), new Service("EcmManagement", true), new Service(
 						"ExternalSecurity", true));
-		
 		expectedResult.setStatusList(testStatusServiceList);
-		
 		Result actualResult = SoapUIHandler.runTestSuite();
-		assertEquals(expectedResult.getStatusList().get(0).getIsUp(), actualResult.getStatusList().get(0).getIsUp());
-	}
 
+		boolean flag = true;
+		System.out.println("Now comparing actual result against expected result.");
+		//Only works as intended if the status list in the actualResult object is ordered the same as that in the expectedResult object.
+		for (int i = 0; i < actualResult.getStatusList().size(); i++) {
+			if ((expectedResult.getStatusList().get(i).getName() != actualResult
+					.getStatusList().get(i).getName())
+					&& (expectedResult.getStatusList().get(i).getIsUp() != actualResult
+							.getStatusList().get(i).getIsUp())) {
+				flag = false;
+				System.out.println("Error! " + actualResult.getStatusList().get(i).getName() + " actual result does not match expected result.");
+			}
+		}
+		
+		assertTrue("Expected result does not match actual result.", flag == true);
+	}
 }
