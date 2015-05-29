230395edd1ed40394355838afc84a0f08c9f2a47
Daniel Vu <duongxvu@gmail.com>
2015-05-29 16:15:42 -0400

    Removed logback and jcl-over-sjf4 dependencies

--- src/ca/lrc/controllers/FileUploadController.java
+++
@@ -1,8 +0,0 @@
-package ca.lrc.controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class FileUploadController {
-	
-}
--- src/ca/lrc/controllers/HomeController.java
+++ src/ca/lrc/controllers/HomeController.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import javax.servlet.ServletContext;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,8 +21,7 @@ import com.eviware.soapui.impl.wsdl.WsdlProject;
 @Controller
 public class HomeController {
 	// DAO dao = new DAO();
-	final static Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	private SoapUIHandler handler = new SoapUIHandler();
 
 	@Autowired
@@ -48,48 +45,51 @@ public class HomeController {
 
 	@RequestMapping("new-uptime-report")
 	public String showDisplayReport(Model model) throws Exception {
-/*		WsdlProject project = new WsdlProject(
-				servletContext.getRealPath("/ServiceUptime-soapui-project.xml"));
-		Report report = handler.runTests(project);
-		System.out.println("Report done running");
-		List<Result> resultList = report.getResultList();
-		for (int i = 0; i < resultList.size(); i++) {
-			System.out.println("\nName: " + resultList.get(i).getName()
-					+ "\n Success flag: " + resultList.get(i).getSuccessFlag()
-					+ "\n Reason for failing: "
-					+ resultList.get(i).getReasonForFailing());
-		}
-		model.addAttribute("resultList", resultList);*/
-		
+		/*
+		 * WsdlProject project = new WsdlProject(
+		 * servletContext.getRealPath("/ServiceUptime-soapui-project.xml"));
+		 * Report report = handler.runTests(project);
+		 * System.out.println("Report done running"); List<Result> resultList =
+		 * report.getResultList(); for (int i = 0; i < resultList.size(); i++) {
+		 * System.out.println("\nName: " + resultList.get(i).getName() +
+		 * "\n Success flag: " + resultList.get(i).getSuccessFlag() +
+		 * "\n Reason for failing: " + resultList.get(i).getReasonForFailing());
+		 * } model.addAttribute("resultList", resultList);
+		 */
+
 		Environment environment = new Environment();
 		model.addAttribute("environment", environment);
 		return "display-report";
 	}
-	
-	@RequestMapping(value="processEnvironmentSelection", method=RequestMethod.GET)
-	public String processEnvironmentSelection(Model model, @ModelAttribute Environment environment) throws Exception {
+
+	@RequestMapping(value = "processEnvironmentSelection", method = RequestMethod.GET)
+	public String processEnvironmentSelection(Model model,
+			@ModelAttribute Environment environment) throws Exception {
 		String userChoice = environment.getSelection();
 		if (userChoice.equals("dev")) {
-			System.out.println("SoapUI project for development environment hasn't been acquired yet.");
+			System.out
+					.println("SoapUI project for development environment hasn't been acquired yet.");
 		}
-		
+
 		else if (userChoice.equals("uat")) {
-			model.addAttribute("resultList", createUptimeReport("/ServiceUptime-soapui-project.xml"));
+			model.addAttribute("resultList",
+					createUptimeReport("/ServiceUptime-soapui-project.xml"));
 		}
-		
+
 		else if (userChoice.equals("prod")) {
-			System.out.println("SoapUI project for production environment hasn't been acquired yet.");
+			System.out
+					.println("SoapUI project for production environment hasn't been acquired yet.");
 		}
-		
+
 		else {
-			System.out.println("How in the hell did you trigger this statement?");
+			System.out
+					.println("How in the hell did you trigger this statement?");
 		}
 		return "display-report";
 	}
-	
+
 	public List<Result> createUptimeReport(String url) throws Exception {
-		WsdlProject project = new WsdlProject(
-				servletContext.getRealPath(url));
+		WsdlProject project = new WsdlProject(servletContext.getRealPath(url));
 		Report report = handler.runTests(project);
 		System.out.println("Report done running");
 		List<Result> resultList = report.getResultList();
@@ -101,6 +101,5 @@ public class HomeController {
 		}
 		return resultList;
 	}
-	
-	
+
 }
